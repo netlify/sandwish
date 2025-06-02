28503f34ce276c5860f2493edacf83aca591c551
Philippe Serhal <philippe.serhal@netlify.com>
2025-06-02 16:15:01 -0400

    fix(deps): bump to latest @netlify/vite-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,5 @@ import { defineConfig } from "vite";
 import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
-  plugins: [
-    netlify({
-      middleware: true
-    })
-  ]
+  plugins: [netlify()],
 });
