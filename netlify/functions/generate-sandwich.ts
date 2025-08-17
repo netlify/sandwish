04e4fbbc265b9ed4d12f8db2266192936b134365
Eduardo Bouças <mail@eduardoboucas.com>
2025-08-17 16:19:32 +0100

    adjust preview sizing

--- netlify/functions/generate-sandwich.ts
+++ netlify/functions/generate-sandwich.ts
@@ -7,7 +7,7 @@ import {
 } from "../../src/ingredients";
 
 const CANVAS_SIZE = 600;
-const IMAGE_FACTOR = 0.9;
+const IMAGE_FACTOR = 0.8;
 const SPACING_FACTOR = 0.06;
 
 export default async (req: Request) => {
